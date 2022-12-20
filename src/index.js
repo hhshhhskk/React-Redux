7dcc15eddfdb41a8bebf68b5d47d5fc5799933f9
hhshhhskk <hhshhhskk@naver.com>
2022-12-20 19:36:23 +0900

    Add Delete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import App from "./components/App";
-import store from "./store.js"
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store.js
+++ src/store.js
@@ -3,33 +3,36 @@ import { createStore } from "redux";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-export const addToDo = text => {
-    return {
-        type: ADD,
-        text
-    };
+const addToDo = text => {
+  return {
+    type: ADD,
+    text
+  };
 };
 
-export const deleteToDo = id => {
-    return {
-        type: DELETE,
-        id
-    };
+const deleteToDo = id => {
+  return {
+    type: DELETE,
+    id: parseInt(id)
+  };
 };
 
 const reducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo !== action.id);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+        return state.filter(toDo => toDo.id !== action.id);
+    default:
+      return state;
+  }
 };
 
 const store = createStore(reducer);
 
-store.subscribe();
+export const actionCreators = {
+    addToDo,
+    deleteToDo
+};
 
 export default store;
